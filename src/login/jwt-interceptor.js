0a508a58b6c3fb2275706f54187ef4924c31c946
JU_MYUNGHOON <myunghoonju@gmail.com>
2021-08-20 01:40:13 +0900

    Update jwt-interceptor.js

--- src/login/jwt-interceptor.js
+++ src/login/jwt-interceptor.js
@@ -9,9 +9,9 @@ export function jwtInterceptor () {
  /*   const isLoggedIn = account.token
     const isApiUrl = request.url.startsWith(process.env.VUE_APP_FACEBOOK_API_URL)
     if (isLoggedIn && isApiUrl) {
-      request.headers.common.Authorization = `Bearer EAAHQL2kuNyoBAMnw6LenJAq0Fj1ZChwVmnhesSuMfzmrqTZAaERxU1yExaZAV5ZAPHDwsh8X0eZAFZBft9zXibh9bDoUOjKs116wYmuOYc76ZBdCT5cbSRvsulbUkvLPX7LuPQCikXZCCymQhphrwLUrgZBtNuo9IPeoOEw3On2mNl7KCP7vLR5j5tjovlYQ8sPjZAMcivpqzJmcnH9u44fpPtRfh3btZB7Rjai5MV9bXs0cAZDZD`;
+      request.headers.common.Authorization = `Bearer `;
     }*/
-    request.headers.common.Authorization = `Bearer EAAHQL2kuNyoBAMnw6LenJAq0Fj1ZChwVmnhesSuMfzmrqTZAaERxU1yExaZAV5ZAPHDwsh8X0eZAFZBft9zXibh9bDoUOjKs116wYmuOYc76ZBdCT5cbSRvsulbUkvLPX7LuPQCikXZCCymQhphrwLUrgZBtNuo9IPeoOEw3On2mNl7KCP7vLR5j5tjovlYQ8sPjZAMcivpqzJmcnH9u44fpPtRfh3btZB7Rjai5MV9bXs0cAZDZD`;
+    request.headers.common.Authorization = `Bearer `;
     return request;
   });
 }
